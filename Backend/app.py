32dda37837c2b16f72ec3f3d9e839be65c06740d
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-08-15 17:07:51 +0530

    Connect the firestore

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,16 @@
+import firebase_admin
 from fastapi import FastAPI
+from firebase_admin import credentials, firestore
 import uvicorn
 
 app = FastAPI()
+
+# Initialize Firebase
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+# Get Firestore client
+db = firestore.client()
 
 @app.get("/")
 def read_root():
@@ -13,3 +22,4 @@ def status():
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
