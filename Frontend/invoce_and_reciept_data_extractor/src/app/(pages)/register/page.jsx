ee8a825b40c9d217f915588421d2ab851ab0d416
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-08-29 00:55:41 +0530

    Add tailwind css

---
+++ Frontend/invoce_and_reciept_data_extractor/src/app/(pages)/register/page.jsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-2xl font-bold">Hello Tailwind CSS v4!</h1>
+    </div>
+  );
+}
